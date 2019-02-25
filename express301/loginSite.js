68ecd8109af6a0f6c00715cadeb121df3b8fecfc
unknown <nbatre1@yahoo.com>
2019-02-25 15:37:01 -0600

    keep moving forward

--- express301/loginSite.js
+++ express301/loginSite.js
@@ -18,4 +18,20 @@ app.get('/',(req,res,next) =>{
     res.send("Sanity Check");
 });
 
+app.get('/login',(req,res,next) =>{
+    res.render('login');
+})
+
+app.post('/process_login',(req,res,next) =>{
+    //req.body is made by urlencoded,which parses the http message for sent data
+    const password = req.body.password;
+    const username = req.body.username;
+// the the db to see if user credentials are valid
+// if they are  valid  send it to the welcome page
+// save it with a cookie or a session
+
+    
+    res.json(req.body);
+})
+
 app.listen(3000);
