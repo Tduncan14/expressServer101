e2016e2fde6f593922febc0dae770d62c45d19db
unknown <nbatre1@yahoo.com>
2019-03-01 23:54:50 -0600

    keep moving forward

--- express301/loginSite.js
+++ express301/loginSite.js
@@ -73,6 +73,60 @@ app.get('/welcome',(req,res,next) =>{
     res.render('welcome',{
         username:req.cookies.username
     })
+});
+
+// in a route, anytime something has : infront it is a wildcard
+//wildcard, will match anything in that slot
+app.param('id',(req,res,next,id) =>{
+     console.log("params called :",id); 
+     // if id has something to do with stories...
+     // if id has something to do with a blog
+     next();
+})
+
+// in a route, anytime something has a : in front it is a wild card
+// a wildcard will match anything in that slot
+app.get('/story/:id',(req,res) =>{
+   // the req.parms object always exist
+   // it will have a property for each wild card inn the route
+   res.send(`<h1> Story ${req.params.id} </h1>`)
+  // res.send(`<h1> story 1 </h1>`)
+});
+
+// will never run because req,res works only when it finds the first route that muchs
+//app.get('/story/:link',(req,res) =>{
+
+ //   res.send(`<h1> stort ${req.params.storyId} - ${req.params.link}`)
+//})
+
+
+//app.get('/story/1',(req,res) =>{
+  //  res.send('<h1> hello </h1>')
+//})
+
+//app.get('/story/2',(req,res) =>{
+  //  res.send('<h1> hello story2 </h1>')
+//})
+
+//app.get('/story/3',(req,res) =>{
+  //  res.send('<h1> hello story3 </h3>')
+//})
+
+app.get('/statement',(req,res) =>{ 
+    
+// This will render the statement in the browser which we dont want
+// app has a download method that takes 2 args:
+// 1 is the the file name
+// What you want the file name to download as
+
+// this allows you to download the file
+// download is allowing you to set the headers
+
+// content-disposition(name)attachment, with the fiename to the 2nd arg
+res.download(path.join(__dirname,'userStatements/statement.png'),'yourStatement.png');
+
+ //res.sendFile(path.join(__dirname,'userStatements/statement.png'))
+
 })
 
 app.get('/logout',(req,res,next) =>{
